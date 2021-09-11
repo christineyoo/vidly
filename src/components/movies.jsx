718ec7aa21ba1915897cfd30f8caa9a37dd4e6b9
Christine Yoo <christinecho.s@gmail.com>
2021-09-10 21:02:21 -0700

    implemented deleting movies from the UI

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,40 +3,50 @@ import { getMovies } from '../services/fakeMovieService';
 
 class Movies extends React.Component {
   state = {
-    movies: getMovies(),
-    count: 9
+    movies: getMovies()
   };
 
-  handleDelete = movie => {
-      this.setState({ count: this.state.count - 1 });
-      const removedMovieIndex = this.state.movies.indexOf(movie);
-      this.setState({ movies: this.state.movies.filter((movie, i) => i !== removedMovieIndex)})
-  }
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
 
   render() {
+      const { length: count } = this.state.movies;
+
+      if (count === 0) return <p>There are no movies in the database</p>
+
     return (
       <>
-        {this.state.count > 0 ? <p>Showing {this.state.count} movies in the database.</p> : <p>There are no movies in the database</p>}
+        <p>Showing {count} movies in the database.</p>
         <table className='table'>
-            <thead>
-                <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Genre</th>
-                <th scope="col">Stock</th>
-                <th scope="col">Rate</th>
-                </tr>
-            </thead>
-            <tbody>
+          {/* <thead> */}
+            <tr>
+              <th scope='col'>Title</th>
+              <th scope='col'>Genre</th>
+              <th scope='col'>Stock</th>
+              <th scope='col'>Rate</th>
+            </tr>
+          {/* </thead> */}
+
+          <tbody>
             {this.state.movies.map((movie) => (
-                <tr key={movie._id}>
-                    <td>{movie.title}</td>
-                    <td>{movie.genre.name}</td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate}</td>
-                    <td><button type="button" className="btn btn-danger" onClick={() => this.handleDelete(movie)}>Delete</button></td>
-                </tr>
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className='btn btn-danger'
+                    onClick={() => this.handleDelete(movie)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
             ))}
-            </tbody>
+          </tbody>
         </table>
       </>
     );
@@ -44,4 +54,3 @@ class Movies extends React.Component {
 }
 
 export default Movies;
-
