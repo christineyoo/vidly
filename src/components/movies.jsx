b54466f3583ee75ccc186231698ee656a0b51b47
Christine Yoo <christinecho.s@gmail.com>
2021-09-22 22:44:07 -0700

    added pagination and installed lodash

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like';
+import Pagination from './common/pagination';
+import { getMovies } from '../services/fakeMovieService';
 
 export default function Movies() {
   const [movies, setMovies] = useState(getMovies());
+  const [pageSize, setPageSize] = useState(10); //4 movies per page
 
   const handleDelete = (movie) => {
     const updatedMovies = movies.filter((m) => m._id !== movie._id);
@@ -19,6 +21,10 @@ export default function Movies() {
     //In the future, call the backend server here too so the changes are persisted
   };
 
+  const handlePageChange = (page) => {
+    console.log('changed', page);
+  };
+
   const count = movies.length;
   if (count === 0) return <p>There are no movies in the database</p>;
 
@@ -26,14 +32,16 @@ export default function Movies() {
     <>
       <p>Showing {count} movies in the database.</p>
       <table className='table'>
-        <tr>
-          <th scope='col'>Title</th>
-          <th scope='col'>Genre</th>
-          <th scope='col'>Stock</th>
-          <th scope='col'>Rate</th>
-          <th />
-          <th />
-        </tr>
+        <thead>
+          <tr>
+            <th scope='col'>Title</th>
+            <th scope='col'>Genre</th>
+            <th scope='col'>Stock</th>
+            <th scope='col'>Rate</th>
+            <th />
+            <th />
+          </tr>
+        </thead>
 
         <tbody>
           {movies.map((movie) => (
@@ -57,6 +65,11 @@ export default function Movies() {
           ))}
         </tbody>
       </table>
+      <Pagination
+        itemsCount={movies.length}
+        pageSize={pageSize}
+        onPageChange={handlePageChange}
+      />
     </>
   );
 }
